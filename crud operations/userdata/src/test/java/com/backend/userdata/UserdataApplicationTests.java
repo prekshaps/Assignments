1d598b75fcef120b2a4c3ff7fe42aafecb827390
Preksha <P7112764@NESSINBLR.COM>
2021-08-17 20:04:31 +0530

    test added

--- crud operations/userdata/src/test/java/com/backend/userdata/UserdataApplicationTests.java
+++ crud operations/userdata/src/test/java/com/backend/userdata/UserdataApplicationTests.java
@@ -2,12 +2,32 @@ package com.backend.userdata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.azure.data.cosmos.PartitionKey;
+import com.backend.userdata.Repository.UserRepository;
+import com.backend.userdata.model.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 
 @SpringBootTest
 class UserdataApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void givenUserIsCreated_whenCallFindById_thenUserIsFound() {
+        User user = new User();
+        user.setId(1);
+        user.setFirstName("tony");
+        user.setLastName("jony");
+        user.setEmailId("tony@gmail.com");
+
+        userRepository.save(user);
+        User retrievedData = userRepository.findById((long) 1, new PartitionKey(null))
+            .orElse(null);
+
+    }
 
 }
